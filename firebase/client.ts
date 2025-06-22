5d96d514dd6a2a393bc0b09a4be963e7e688349f
Anshdeep singh <121263870+anshdeep0504@users.noreply.github.com>
2025-06-22 09:37:45 +0530

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-MJ_YpV5cHPslvqYxDVsD9zh9t7PyT2c",
-  authDomain: "fir-3567d.firebaseapp.com",
-  projectId: "fir-3567d",
-  storageBucket: "fir-3567d.firebasestorage.app",
-  messagingSenderId: "38636565861",
-  appId: "1:38636565861:web:c892bd27595f4a1412dd8c",
-  measurementId: "G-BRWP9VJ4FF"
+  //hidden
 };
 
 // Initialize Firebase
